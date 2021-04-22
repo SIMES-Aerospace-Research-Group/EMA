f5c024a76c10a6acc13e8a30d6f22b9ee9feb71c
Rodrigo Flores <rodrigo.flores.12.07@gmail.com>
2021-04-21 22:13:11 -0400

    Update: uploading a few of configurations on main.py

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 ## Importing system libraries
-import sys
-import wiringpi
-import spidev
 from numpy import median
 import wiringpi as wipi
-import config
 from time import *
+import wiringpi
+import config
+import spidev
+import sys
 
 ## Importing Classes and Methods from local files
+from modules import SHARPPM10
 from modules import MCP3008
 from modules import HDC1080
 from modules import LCD1602
 from modules import RGB_LED
-from modules import SHARPPM10
 
 # degree_symbol = u"\u00b0"
 
-## Pines setup
+## Pines GRPIO setup
 sharp_pin = 21
 sharp_channel = 1
 
 redPin = 11
 greenPin = 13
 bluePin = 15
+# Ground = 9
 
 ## Setup of modules
 initLCD()
@@ -36,37 +37,40 @@ ADC = MCP3008(0, 0) # CE0
 
 sharpPM10 = sharpPM10(led_pin=sharp_pin, pm10_pin=sharp_channel, adc=ADC)
 
+
+yellowOn()
+limit = 30
+print('\t[ ? ] Tomando datos ...')
+
+"""
 yellowOn() # Pin turned to yellow
 for i in (30): # 0.2 seconds * 30 = 6 seconds
-    printLCD('[ ok ] Tomando datos .  ')
+    printLCD('[ ? ] Tomando datos .  ')
     sleep(.2)
-    printLCD('[ ok ] Tomando datos .. ')
+    printLCD('[ ? ] Tomando datos .. ')
     sleep(.2)
-    printLCD('[ ok ] Tomando datos ...')
+    printLCD('[ ? ] Tomando datos ...')
     sleep(.2)
 yellowOff() # Turn off the yellow led
 
-try:
-    # Printing on display
-    lcd.lcd_print("Success")
+printLCD("[ ok ]") # Printing on display
 
-    # ADDING RGB LED ... CHECK ADAFRUIT RGB DOCUMENTATION
-    greenOn()
+greenOn() # ADDING RGB LED ... CHECK ADAFRUIT RGB DOCUMENTATION
 
-    dust_density = sharpPM10.read() # Reading dust density with sharpPM10
+"""
 
-    # Printing out the data
-    printLCD(f'{HDCtemp(2)}  {HDChum(2)}')
-
-    # Printing on display
-    printLCD([
-        (f"Hum: {humidity}"),
-        (f"Tem: {temperature}"),
-        (f"Polvo: {dust_density}")
-    ])
-
-except:
-    lcd.lcd_print(['Process Unsuccessful'])
-    turnOff(redPin)
-    turnOff(greenPin)
-    turnOff(bluePin)
+while True:
+    try:
+        # Printing out the display
+        printLCD(
+            (f"Hum: {HDCtemp(2)}"), # Temperature
+            (f"Tem: {HDChum(2)}\n"), # Humidity
+            (f"Polvo: {sharpPM10.read()}) # Dust density
+        )
+        sleep(3)
+    except:
+        printLCD('Process Unsuccessful')
+        sleep(3)
+        turnOff(redPin)
+        turnOff(greenPin)
+        turnOff(bluePin)
