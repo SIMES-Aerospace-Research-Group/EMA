6b35f350ee3641acf1d612c47580213192669f67
Carlos <yolitopinto@gmail.com>
2021-05-28 17:24:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,4 @@ sharpPM10 = sharpPM10(led_pin=21, pm10_pin=1, adc=ADC)
 print(f'MCP read_output          : {ADC.read()}')
 print(f'SHARP output read        : {sharpPM10.read()}')
 print(f'SHARP output readSequence: {sharpPM10.readSequence()}')
+
