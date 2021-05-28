605b767e304367715fe041719e9bdc1c3bfad8b3
Carlos <yolitopinto@gmail.com>
2021-05-28 17:28:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from modules.SHARP_PM10 import sharpPM10
 ADC = mcp3008(0, 0) # CE0
 sharpPM10 = sharpPM10(led_pin=21, pm10_pin=1, adc=ADC) 
 
-print(f'MCP read_output          : {ADC.read()}')
-print(f'SHARP output read        : {sharpPM10.read()}')
-print(f'SHARP output readSequence: {sharpPM10.readSequence()}')
+print(f'MCP read_output          : { ADC.read() }')
+print(f'SHARP output read        : { float(sharpPM10.read()) * 100 }')
+print(f'SHARP output readSequence: { sharpPM10.readSequence() }')
 
